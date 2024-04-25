6a24a10a0fd9a7d1a828e9fb2c593a6882ac5ca3
Antonio Cuesta <56271718+zyanuro@users.noreply.github.com>
2024-04-25 16:32:11 +0200

    strlcpy include
    norminette passed

---
+++ libft/ft_strlcat.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancuesta <ancuesta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 16:32:41 by ancuesta          #+#    #+#             */
+/*   Updated: 2024/04/25 16:19:12 by ancuesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+/*size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	int	counter;
+
+	counter = 0;
+}
+
+	int main()
+	{
+	char src[] = "Esto es de la primera cadena";
+	char dest[20] = "y la segunda cadena"; // Tamaño del buffer de destino
+    size_t dstsize = 20;
+
+    ft_strlcat(dest, src, dstsize); 
+    
+    return 0;   
+} */
