d4aefe770840059aa6170f3e7f76af828dae8d52
Antonio Cuesta <56271718+zyanuro@users.noreply.github.com>
2024-04-25 16:26:40 +0200

    Hasta strlcpy
    con morminette pasada en todas y fclean

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: ancuesta <ancuesta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:38:50 by ancuesta          #+#    #+#             */
-/*   Updated: 2024/04/25 16:08:20 by ancuesta         ###   ########.fr       */
+/*   Updated: 2024/04/25 16:23:41 by ancuesta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	size_t	i;
 	unsigned char		*uchardst;
 	const unsigned char	*ucharsrc;
+	size_t				i;
 
 	if (!dst && !src)
 		return (0);
