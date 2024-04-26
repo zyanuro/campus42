046eb6908c324155573bcb271c0cc7af8547e65b
Antonio Cuesta <56271718+zyanuro@users.noreply.github.com>
2024-04-26 19:40:16 +0200

    commit hasta tolower

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,28 +6,84 @@
 /*   By: ancuesta <ancuesta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 16:32:41 by ancuesta          #+#    #+#             */
-/*   Updated: 2024/04/25 16:19:12 by ancuesta         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:29:19 by ancuesta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include "libft.h"
 
-/*size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	int	counter;
-
-	counter = 0;
+    size_t  i;
+    size_t  j;
+    i = 0;
+    j = 0;
+    while (dst[i] && i < dstsize)
+        i++;
+    while (src[j] && (i + j + 1) < dstsize)
+    {
+        dst[i + j] = src[j];
+        j++;
+    }
+    if (i < dstsize)
+        dst[i + j] = '\0';
+    return (i + ft_strlen(src));
 }
 
-	int main()
-	{
-	char src[] = "Esto es de la primera cadena";
-	char dest[20] = "y la segunda cadena"; // Tamaño del buffer de destino
-    size_t dstsize = 20;
+/*int	main(void)
+{
+	char		destino[] = "Hola ";
+	const char	fuente[] = "Mundo";
+	size_t		dest_size;
+	size_t		resultado;
 
-    ft_strlcat(dest, src, dstsize); 
+	dest_size = 3;
+	resultado = ft_strlcat(destino, fuente, dest_size);
+
+    printf("La cadena concatenada es: %zu\n", resultado);		
+	printf("La cadena concatenada es: %s\n", destino);
+	
+	return (0);
+}*/
+
+/*size_t	ft_strlcat(char *dest, const char *src, size_t dstsize) {
+    size_t dstlen = 0;
+    size_t srclen = 0;
+    size_t i = 0;
     
-    return 0;   
-} */
+    while (dest[dstlen] != '\0')
+        dstlen++;
+    
+    while (src[srclen] != '\0')
+        srclen++;
+    
+    if (dstsize == 0)
+        return srclen;
+    
+    //size_t available = dstsize - dstlen - 1;
+    
+    while (src[i] != '\0') {
+        dest[dstlen + i] = src[i];
+        i++;
+    }
+    
+    dest[dstlen + i] = '\0';    
+    return dstlen + srclen;
+}
+
+int	main(void)
+{
+	char		destino[] = "lorem ipsum dolor sit amet - ";
+	const char	fuente[] = "abcdefghi";
+	size_t		dest_size;
+	size_t		resultado;
+
+	dest_size = 5;
+	resultado = ft_strlcat(destino, fuente, dest_size);
+		
+	printf("La cadena concatenada es: %s\n", destino);
+	
+	return (0);
+}*/
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: ancuesta <ancuesta@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 13:27:35 by ancuesta          #+#    #+#             */
-/*   Updated: 2024/04/25 15:54:16 by ancuesta         ###   ########.fr       */
+/*   Updated: 2024/04/26 19:24:44 by ancuesta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,8 @@ void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-//size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
+int     ft_toupper(int c);
+int     ft_tolower(int c);
+
 #endif
