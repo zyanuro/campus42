d58ee9ea191627cc04fce13fd7825f1ca2144a20
Antonio Cuesta <56271718+zyanuro@users.noreply.github.com>
2024-04-30 12:57:35 +0200

    hasta strrchr
    sin norminette

---
+++ libft/libft/old_ft_strrchr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancuesta <ancuesta@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 11:59:37 by ancuesta          #+#    #+#             */
+/*   Updated: 2024/04/30 12:21:10 by ancuesta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+char    *ft_strchr(const char *s, int c)
+{
+    int i;
+    i = ft_strlen(s);
+    printf("%d\n", i);
+    c = (char)c;
+    if (c == '\0')
+        return  ((char *)&s[ft_strlen(s)]);
+    while (s[i] >= 0)
+    {
+        if (s[i] == c)
+        return ((char *)&s[i]);
+
+        i--;
+    }
+    return  (NULL);
+}
+
+/*int main(void)
+{
+    char str[] = "testing";
+    char character = 'i';
+    char *result = ft_strchr(str, character);
+    if (result != NULL)
+        printf("Resultado: %s\n", result); // Imprime la cadena desde el carácter encontrado
+    else
+        printf("El carácter no se encontró en la cadena.\n");
+    return (0);
+}*/
